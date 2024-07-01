e19cf726a91f81d33fa5beb86877d4ad6c753898
Alexander-Tengborg <Landervich@Gmail.com>
2024-07-02 00:40:09 +0200

    Split the hello world function in 2 functions

--- hello/hello.go
+++ hello/hello.go
@@ -2,22 +2,25 @@ package main
 
 import "fmt"
 
-const spanish = "Spanish"
-const french = "French"
-const russian = "Russian"
-
-const englishHelloPrefix = "Hello, "
-const spanishHelloPrefix = "Hola, "
-const frenchHelloPrefix = "Bonjour, "
-const russianHelloPrefix = "Privyet, "
+const (
+	spanish = "Spanish"
+	french = "French"
+	russian = "Russian"
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix = "Bonjour, "
+	russianHelloPrefix = "Privyet, "
+)
 
 func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
 
-	prefix := englishHelloPrefix
+	return greetingPrefix(language) + name
+}
 
+func greetingPrefix(language string) (prefix string) {
 	switch language {
 	case spanish:
 		prefix = spanishHelloPrefix
@@ -25,9 +28,11 @@ func Hello(name string, language string) string {
 		prefix = frenchHelloPrefix
 	case russian:
 		prefix = russianHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
 
-	return prefix + name
+	return
 }
 
 func main() {
