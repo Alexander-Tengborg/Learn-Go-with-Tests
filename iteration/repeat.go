66b849561bb219070693eb9b17dec4a9bf71f000
Alexander-Tengborg <Landervich@Gmail.com>
2024-07-02 01:07:44 +0200

    Added a benchmark and an example to iteration/repeat

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,5 +1,11 @@
 package iteration
 
-func Repeat(to_repeat string) string{
-	return ""
+// Repeats the given character repatCount amount of times and returns it
+func Repeat(character string, repeatCount int) string {
+	var repeated string
+	for i := 0; i < repeatCount; i++ {
+		repeated += character
+	}
+
+	return repeated
 }
