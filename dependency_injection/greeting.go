5633f5b24431b276b787da2de5d9832b35daca03
Alexander-Tengborg <Landervich@Gmail.com>
2024-07-03 00:28:51 +0200

    finished dependency injection

--- dependency_injection/greeting.go
+++ dependency_injection/greeting.go
@@ -1,7 +1,20 @@
-package dependencyinjection
+package main
 
-import "io"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
 
 func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
 
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "Linda")
+}
+
+func main() {
+	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreeterHandler)))
 }
