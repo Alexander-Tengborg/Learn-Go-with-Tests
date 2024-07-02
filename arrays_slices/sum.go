c58c6875084bcc01e29813c3b3f547da0b034e9c
Alexander-Tengborg <Landervich@Gmail.com>
2024-07-02 21:28:41 +0200

    finished arrays and slices

--- arrays_slices/sum.go
+++ arrays_slices/sum.go
@@ -7,3 +7,27 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		sum := 0
+		if len(numbers) > 1 {
+			sum = Sum(numbers[1:])
+		}
+		sums = append(sums, sum)
+	}
+
+	return sums
+}
--- arrays_slices/sum_test.go
+++ arrays_slices/sum_test.go
@@ -1,16 +1,58 @@
 package arraysslices
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
-	t.Run("slice of 5", func(t *testing.T) {
-		numbers := []int{1, 2, 3, 4, 5}
+	numbers := []int{1, 2, 3, 4, 5}
 
-		got := Sum(numbers)
-		want := 15
+	got := Sum(numbers)
+	want := 15
 
-		if got != want {
-			t.Errorf("got %d want %d given %d", got, want, numbers)
+	if got != want {
+		t.Errorf("got %d want %d given %d", got, want, numbers)
+	}
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3,9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	checkSums := func(t testing.TB, got[]int, want[]int) {
+		t.Helper()
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
 		}
+	}
+	t.Run("gets the tail slice of 2 slices of length 2", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+		
+		checkSums(t, got, want)
+	})
+
+	t.Run("gets the tail slice of 2 slices of length 3", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2, 5}, []int{0, 9, 3})
+		want := []int{7, 12}
+		
+		checkSums(t, got, want)
+
+	})
+
+	t.Run("gets the tail slice of 1 slice of length 0, and 1 of length 2", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{0, 9})
+		want := []int{0, 9}
+		
+		checkSums(t, got, want)
 	})
 }
