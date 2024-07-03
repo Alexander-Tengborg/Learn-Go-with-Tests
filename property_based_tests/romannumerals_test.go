114ee4a780fad44af9416a8613d9e79cd7256d35
Alexander-Tengborg <Landervich@Gmail.com>
2024-07-03 17:26:31 +0200

    roman numerals progress

--- property_based_tests/romannumerals_test.go
+++ property_based_tests/romannumerals_test.go
@@ -3,12 +3,26 @@ package romannumerals
 import "testing"
 
 func TestRomanNumerals(t *testing.T) {
-	t.Run("converting 1 to roman numerals", func(t *testing.T) {
-		got := ConvertToRoman(1)
-		want := "I"
+	cases := []struct {
+		Description string
+		Arabic      int
+		Want        string
+	}{
+		{"1 gets converted to I", 1, "I"},
+		{"2 gets converted to II", 2, "II"},
+		{"3 gets converted to III", 3, "III"},
+		{"4 gets converted to IV", 4, "IV"},
+		{"5 gets converted to V", 5, "V"},
+	}
 
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
-	})
+	for _, test := range cases {
+		t.Run(test.Description, func(t *testing.T) {
+			got := ConvertToRoman(test.Arabic)
+	
+			if got != test.Want {
+				t.Errorf("got %q, want %q", got, test.Want)
+			}
+		})
+	}
+
 }
