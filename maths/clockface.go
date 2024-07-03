e5cacd3c5a7ea92af11e9fd2f20cc9d0e7073dcc
Alexander-Tengborg <Landervich@Gmail.com>
2024-07-03 21:16:03 +0200

    Added a minute hand to the clock

--- maths/clockface.go
+++ maths/clockface.go
@@ -18,9 +18,9 @@ type Point struct {
 
 func SecondHand(t time.Time) Point {
 	p := secondHandPoint(t)
-	p = Point{p.X * secondHandLength, p.Y * secondHandLength}   // scale
-	p = Point{p.X, -p.Y}            // flip
-	p = Point{p.X + clockCentreX, p.Y + clockCentreY} // translate
+	p = Point{p.X * secondHandLength, p.Y * secondHandLength} // scale
+	p = Point{p.X, -p.Y}                                      // flip
+	p = Point{p.X + clockCentreX, p.Y + clockCentreY}         // translate
 
 	return p
 }
@@ -29,14 +29,25 @@ func secondsInRadians(t time.Time) float64 {
 	return (math.Pi / (30 / float64(t.Second())))
 }
 
+func minutesInRadians(t time.Time) float64 {
+	return (math.Pi / (30 / float64(t.Minute()))) + secondsInRadians(t)/60
+}
+
 func simpleTime(hours int, minutes int, seconds int) time.Time {
 	return time.Date(312, time.October, 28, hours, minutes, seconds, 0, time.UTC)
 }
 
-func secondHandPoint(t time.Time) Point {
-	angle := secondsInRadians(t)
+func angleToPoint(angle float64) Point {
 	x := math.Sin(angle)
 	y := math.Cos(angle)
 
 	return Point{x, y}
+}
+
+func secondHandPoint(t time.Time) Point {
+	return angleToPoint(secondsInRadians(t))
+}
+
+func minuteHandPoint(t time.Time) Point {
+	return angleToPoint(minutesInRadians(t))
 }
